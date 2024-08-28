574aec4904d435c8fab10fafa5fc150a6c0dd575
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2024-08-28 14:07:58 -0600

    commit

--- frozen_lake.py
+++ frozen_lake.py
@@ -3,9 +3,9 @@
 import gymnasium as gym
 import numpy as np
 
-def run(episodes, is_training=True, render=False):
+def run(episodes):
 
-    env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human' if render else None)
+    env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human')
     q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
 
     learning_rate_a = 0.9 
@@ -17,17 +17,15 @@ def run(episodes, is_training=True, render=False):
         state = env.reset()[0] 
         terminated = False      
         truncated = False      
-
         while(not terminated and not truncated):
-            if is_training and rng.random() < epsilon:
+            if rng.random() < epsilon:
                 action = env.action_space.sample() 
             else:
                 action = np.argmax(q[state,:])
 
             new_state,reward,terminated,truncated,_ = env.step(action)
 
-            if is_training:
-                q[state,action] = q[state,action] + learning_rate_a * (reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action])
+            q[state,action] = q[state,action] + learning_rate_a * (reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action])
 
             state = new_state
 
