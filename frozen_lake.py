74c575a801f75c500f862487b69659ab39e26712
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2024-08-28 20:32:57 -0600

    commit

--- frozen_lake.py
+++ frozen_lake.py
@@ -2,35 +2,62 @@
 
 import gymnasium as gym
 import numpy as np
+import matplotlib as plt
 
 def run(episodes):
 
-    env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human')
+    # Initializes the Environment
+    env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=False, render_mode='human')
+
+    # Create a Q-Learning Table
     q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
 
-    learning_rate_a = 0.9 
-    discount_factor_g = 0.9
+    # Define Parameters
+    learning_rate = 0.9 
+    discount_factor = 0.92
     epsilon = 0.05
-    rng = np.random.default_rng()   # random number generator
+    random_number = np.random.default_rng()
 
     for i in range(episodes):
+        # Starting values
         state = env.reset()[0] 
-        terminated = False      
-        truncated = False      
+        terminated = False # When the agent falls or reaches the goal
+        truncated = False  # When actions are greater than 200
+        number_of_steps = 0
+         
         while(not terminated and not truncated):
-            if rng.random() < epsilon:
+            # Use Epsilon-Greedy Exploration
+            if random_number.random() < epsilon:
+                # Random Action
                 action = env.action_space.sample() 
+                print('------------------------')
+                print('random action')
             else:
                 action = np.argmax(q[state,:])
+                print('greedy action')
 
-            new_state,reward,terminated,truncated,_ = env.step(action)
+            number_of_steps += 1
+            next_state,reward,terminated,truncated,info = env.step(action)
 
-            q[state,action] = q[state,action] + learning_rate_a * (reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action])
+            if reward == 0 and terminated == True:
+                reward = -10
+            elif reward == 1:
+                reward = 100
+            reward += -0.1
 
-            state = new_state
+            print(next_state,reward,terminated,truncated)
+            print(q[state,action])
+            print('------------------------')
+
+
+            q[state,action] = q[state,action] + learning_rate * (reward + discount_factor * np.max(q[next_state,:]) - q[state,action])
+
+            state = next_state
 
     env.close()
 
+    
+
 
 if __name__ == '__main__':
-    run(1000)
+    run(3)
